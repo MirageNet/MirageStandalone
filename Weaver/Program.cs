51fd081e8534cdbd51d5c71906ec980894a8a7d9
James Frowen <jamesfrowendev@gmail.com>
2021-09-22 22:21:40 +0100

    adding weaver csproj

---
+++ Weaver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Weaver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
