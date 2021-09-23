0bef16afdc3cc9d8baafade4efe30c2a35a4aa63
James Frowen <jamesfrowendev@gmail.com>
2021-09-23 14:46:37 +0100

    stopping null ref spam

--- Weaver/Extensions.cs
+++ Weaver/Extensions.cs
@@ -1,6 +1,6 @@
+using Mono.Cecil;
 using System;
 using System.Linq;
-using Mono.Cecil;
 
 namespace Mirage.Weaver
 {
@@ -174,7 +174,9 @@ namespace Mirage.Weaver
 
                 try
                 {
-                    parent = parent.Resolve().BaseType;
+                    TypeDefinition resolved = parent.Resolve();
+                    if (resolved == null) { return false; }
+                    parent = resolved.BaseType;
                 }
                 catch
                 {
