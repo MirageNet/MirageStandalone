2dd515266b7879b5dc1305d6754ff4e6010a1845
Pavel Hodoval <pajah14@gmail.com>
2021-10-04 19:57:06 +0200

    feat: added StandaloneRunner

--- Mirage.Examples/Program.cs
+++ Mirage.Examples/Program.cs
@@ -1,79 +1,24 @@
 using System;
-using System.Linq;
-using System.Reflection;
 using Mirage;
-using Mirage.Examples;
-using UnityEngine;
+using Mirage.Standalone;
 
 namespace MirageHostExample
 {
     [NetworkMessage]
     public struct HelloMessage { }
 
-    internal class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
-            RunInitialize();
+            var runner = new StandaloneRunner();
+            NetworkServer server = runner.AddServer();
+            NetworkClient client = runner.AddClient();
 
-            while (true)
-            {
+            server.StartServer();
+            client.Connect("localhost");
 
-                Console.WriteLine("..........Mirage Examples.........\r");
-                Console.WriteLine("Please choose one...\n");
-
-                Console.WriteLine("Option 1: headless Server.");
-
-                int option = Convert.ToInt32(Console.ReadLine());
-
-                switch (option)
-                {
-                    case 1:
-
-                        Console.WriteLine("Please type in command to fire headless example. type -help for info");
-
-                        string command = Console.ReadLine();
-
-                        if (!string.IsNullOrEmpty(command) && command.Contains("-help"))
-                        {
-                            Console.WriteLine("--==Mirage HeadlessClients Benchmark==--");
-                            Console.WriteLine(
-                                "Please start your standalone application with the -nographics and -batchmode options");
-                            Console.WriteLine("Also provide these arguments to control the autostart process:");
-                            Console.WriteLine("-server (will run in server only mode)");
-                            Console.WriteLine("-client 1234 (will run the specified number of clients)");
-                            Console.WriteLine(
-                                "-transport tcp (transport to be used in test. add more by editing HeadlessBenchmark.cs)");
-                            Console.WriteLine("-address example.com (will run the specified number of clients)");
-                            Console.WriteLine("-port 1234 (port used by transport)");
-                            Console.WriteLine("-monster 100 (number of monsters to spawn on the server)");
-
-                            goto case 1;
-                        }
-
-                        var headless = new HeadlessPerformance(command);
-
-                        break;
-                    default:
-                        Console.WriteLine("Incorrect option selected.");
-                        break;
-                }
-            }
-        }
-
-        private static void RunInitialize()
-        {
-            var asm = Assembly.GetExecutingAssembly();
-
-            MethodInfo[] methods = asm.GetTypes()
-                .SelectMany(t => t.GetMethods())
-                .Where(m => m.GetCustomAttributes(typeof(RuntimeInitializeOnLoadMethodAttribute), false).Length > 0)
-                .ToArray();
-
-            foreach (MethodInfo method in methods)
-            {
-                method.Invoke(null, null);
-            }
+            Console.ReadLine();
         }
     }
 }
---
+++ Mirage.Standalone/StandaloneRunner.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Mirage.SocketLayer;
+using Mirage.Sockets.Udp;
+using UnityEngine;
+
+namespace Mirage.Standalone
+{
+    public class StandaloneRunner
+    {
+        private readonly List<NetworkServer> servers = new List<NetworkServer>();
+        private readonly List<NetworkClient> clients = new List<NetworkClient>();
+        private readonly List<UdpSocketFactory> socketFactories = new List<UdpSocketFactory>();
+
+        public StandaloneRunner()
+        {
+            RunInitializeMethods();
+
+            Task.Factory.StartNew(Update, TaskCreationOptions.LongRunning);
+
+            Console.WriteLine("Mirage Standalone Runner initialized");
+        }
+
+        public NetworkServer AddServer(ushort port = 7777)
+        {
+            var socketFactory = new UdpSocketFactory { Port = port };
+            var server = new NetworkServer { SocketFactory = socketFactory };
+
+            servers.Add(server);
+            socketFactories.Add(socketFactory);
+
+            return server;
+        }
+
+        public NetworkClient AddClient()
+        {
+            var socketFactory = new UdpSocketFactory();
+            var client = new NetworkClient { SocketFactory = socketFactory };
+
+            clients.Add(client);
+            socketFactories.Add(socketFactory);
+
+            return client;
+        }
+
+        async void Update()
+        {
+            while (true)
+            {
+                try
+                {
+                    foreach (NetworkServer server in servers)
+                    {
+                        if (!server.Active) continue;
+
+                        server.Update();
+                    }
+
+                    foreach (NetworkClient client in clients)
+                    {
+                        if (!client.Active) continue;
+
+                        client.Update();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+
+                await Task.Delay(5);
+            }
+        }
+
+        static void RunInitializeMethods()
+        {
+            var asm = Assembly.GetEntryAssembly();
+
+            MethodInfo[] methods = asm.GetTypes()
+                .SelectMany(t => t.GetMethods())
+                .Where(m => m.GetCustomAttributes(typeof(RuntimeInitializeOnLoadMethodAttribute), false).Length > 0)
+                .ToArray();
+
+            foreach (MethodInfo method in methods)
+            {
+                method.Invoke(null, null);
+            }
+        }
+    }
+}
