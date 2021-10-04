a99cd1e483adf3a82f127a13a967bccf776ad17b
James Frowen <jamesfrowendev@gmail.com>
2021-10-04 15:50:00 +0100

    adding try/catch to Weaver and adding exit code

--- Weaver/Program.cs
+++ Weaver/Program.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using Mono.Cecil;
-using Mono.Cecil.Cil;
+using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
 
 namespace Mirage.Weaver
 {
@@ -11,18 +11,29 @@ namespace Mirage.Weaver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Mirage Weaver start");
-            string dllPath = args[0];
-            byte[] data = File.ReadAllBytes(dllPath);
-            Assembly asm = Assembly.Load(data);
+            try
+            {
+                Console.WriteLine("Mirage Weaver start");
+                string dllPath = args[0];
+                byte[] data = File.ReadAllBytes(dllPath);
+                var asm = Assembly.Load(data);
 
-            // TODO: use proper Assembly paths 
-            string[] references = asm.GetReferencedAssemblies().Select(a => Path.Combine(Path.GetDirectoryName(dllPath), a.Name)).ToArray();
-            var compiledAssembly = new CompiledAssembly2(dllPath, references, new string[0]);
-            var weaverLogger = new WeaverLogger();
-            var weaver = new Weaver(weaverLogger);
-            AssemblyDefinition assemblyDefinition = weaver.Weave(compiledAssembly);
-            Write(assemblyDefinition, dllPath, compiledAssembly.PdbPath);
+                // TODO: use proper Assembly paths 
+                string[] references = asm.GetReferencedAssemblies().Select(a => Path.Combine(Path.GetDirectoryName(dllPath), a.Name)).ToArray();
+                var compiledAssembly = new CompiledAssembly2(dllPath, references, new string[0]);
+                var weaverLogger = new WeaverLogger();
+                var weaver = new Weaver(weaverLogger);
+                AssemblyDefinition assemblyDefinition = weaver.Weave(compiledAssembly);
+                Write(assemblyDefinition, dllPath, compiledAssembly.PdbPath);
+
+                Environment.ExitCode = 0;
+            }
+            catch (Exception e)
+            {
+                Environment.ExitCode = 1;
+                Console.Error.WriteLine(e);
+                return;
+            }
         }
 
         private static void Write(AssemblyDefinition assemblyDefinition, string dllPath, string pdbPath)
@@ -49,13 +60,13 @@ namespace Mirage.Weaver
     {
         public CompiledAssembly2(string dllPath, string[] references, string[] defines)
         {
-            this.Name = Path.GetFileName(dllPath);
-            this.PdbPath = $"{Path.GetDirectoryName(dllPath)}/{Path.GetFileNameWithoutExtension(dllPath)}.pdb";
+            Name = Path.GetFileName(dllPath);
+            PdbPath = $"{Path.GetDirectoryName(dllPath)}/{Path.GetFileNameWithoutExtension(dllPath)}.pdb";
             byte[] peData = File.ReadAllBytes(dllPath);
-            byte[] pdbData = File.ReadAllBytes(this.PdbPath);
-            this.InMemoryAssembly = new InMemoryAssembly(peData, pdbData);
-            this.References = references;
-            this.Defines = defines;
+            byte[] pdbData = File.ReadAllBytes(PdbPath);
+            InMemoryAssembly = new InMemoryAssembly(peData, pdbData);
+            References = references;
+            Defines = defines;
         }
 
         public InMemoryAssembly InMemoryAssembly { get; }
@@ -77,8 +88,8 @@ namespace Mirage.Weaver
     {
         public InMemoryAssembly(byte[] peData, byte[] pdbData)
         {
-            this.PeData = peData;
-            this.PdbData = pdbData;
+            PeData = peData;
+            PdbData = pdbData;
         }
 
         public byte[] PeData { get; }
