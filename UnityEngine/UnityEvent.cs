1565b74eb3fd4f3f85ae226caa4059c69e21d248
James Frowen <jamesfrowendev@gmail.com>
2021-10-04 18:12:55 +0100

    implementing unityevent

--- UnityEngine/UnityEvent.cs
+++ UnityEngine/UnityEvent.cs
@@ -1,55 +1,83 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 namespace UnityEngine
 {
     public class UnityEvent : UnityEventBase
     {
+        List<Action> handlers = new List<Action>();
 
+        public void AddListener(Action handler)
+        {
+            handlers.Add(handler);
+        }
+        public void RemoveListener(Action handler)
+        {
+            handlers.Remove(handler);
+        }
+        public override void RemoveAllListeners()
+        {
+            handlers.Clear();
+        }
+
+        public void Invoke()
+        {
+            foreach (Action handler in handlers)
+            {
+                handler.Invoke();
+            }
+        }
     }
 
     public class UnityEvent<T> : UnityEventBase
     {
-        public UnityEvent()
-        {
-
-        }
+        List<Action<T>> handlers = new List<Action<T>>();
 
         public void AddListener(Action<T> handler)
         {
-            // TODO: AddListener
+            handlers.Add(handler);
         }
-
         public void RemoveListener(Action<T> handler)
         {
-            // TODO: RemoveListener
+            handlers.Remove(handler);
+        }
+        public override void RemoveAllListeners()
+        {
+            handlers.Clear();
         }
 
-        public void Invoke(T arg)
+        public void Invoke(T arg1)
         {
-            // TODO: Invoke
+            foreach (Action<T> handler in handlers)
+            {
+                handler.Invoke(arg1);
+            }
         }
     }
 
     public class UnityEvent<T0, T1> : UnityEventBase
     {
-        public UnityEvent()
-        {
-
-        }
+        List<Action<T0, T1>> handlers = new List<Action<T0, T1>>();
 
         public void AddListener(Action<T0, T1> handler)
         {
-            // TODO: AddListener
+            handlers.Add(handler);
         }
-
         public void RemoveListener(Action<T0, T1> handler)
         {
-            // TODO: RemoveListener
+            handlers.Remove(handler);
+        }
+        public override void RemoveAllListeners()
+        {
+            handlers.Clear();
         }
 
         public void Invoke(T0 arg1, T1 arg2)
         {
-            // TODO: Invoke
+            foreach (Action<T0, T1> handler in handlers)
+            {
+                handler.Invoke(arg1, arg2);
+            }
         }
     }
 }
