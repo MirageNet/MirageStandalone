0f44422fc7e4804041c952d4a6c1205670025a97
James Frowen <jamesfrowendev@gmail.com>
2021-11-09 18:52:11 +0000

    adding list server project

---
+++ MirageListServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MirageListServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
