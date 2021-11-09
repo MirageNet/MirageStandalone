b68c7471778fbfb6d42f6b3f878d1f15179a35b3
James Frowen <jamesfrowendev@gmail.com>
2021-11-09 19:05:34 +0000

    starting on messages

--- MirageListServer/Program.cs
+++ MirageListServer/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using System.Threading;
+using Mirage;
+using Mirage.Serialization;
+using Mirage.Sockets.Udp;
 
 namespace MirageListServer
 {
@@ -6,7 +9,92 @@ namespace MirageListServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var listServer = new ListServer();
+            listServer.Run();
         }
+    }
+    class ListServer
+    {
+        NetworkServer _server;
+        public void Run()
+        {
+            StartServer();
+            RegisterMessages();
+            Loop();
+        }
+
+        private void StartServer()
+        {
+            _server = new NetworkServer();
+            _server.SocketFactory = new UdpSocketFactory();
+            _server.PeerConfig = new Mirage.SocketLayer.Config
+            {
+                MaxConnections = 1000,
+            };
+
+            _server.StartServer();
+        }
+
+        private void RegisterMessages()
+        {
+            _server.MessageHandler.RegisterHandler()
+        }
+
+        private void Loop()
+        {
+            while (true)
+            {
+                _server.Update();
+                Thread.Sleep(10);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Request to be added to list
+    /// </summary>
+    [NetworkMessage]
+    struct AddServer
+    {
+        public string displayName;
+        public string customAddress;
+        [BitCount(16)] public int port;
+        [VarIntBlocks(7)] public int PlayerCount;
+        [VarIntBlocks(7)] public int MaxPlayerCount;
+    }
+
+    /// <summary>
+    /// Request to be added to list
+    /// </summary>
+    [NetworkMessage]
+    struct UpdateServer
+    {
+        public ulong id;
+        public string displayName;
+        public string customAddress;
+        [BitCount(16)] public int port;
+        [VarIntBlocks(7)] public int PlayerCount;
+        [VarIntBlocks(7)] public int MaxPlayerCount;
+    }
+
+    /// <summary>
+    /// Reply from AddServer with unique id
+    /// </summary>
+    [NetworkMessage]
+    struct AddServerReply
+    {
+        /// <summary>
+        /// Id used to update server in future requests
+        /// </summary>
+        public ulong id;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [NetworkMessage]
+    struct RemoveServer
+    {
+        ulong id;
     }
 }
