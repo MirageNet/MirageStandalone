a1b3f34fdb0cb48df0c0d324312416eb807f48ba
James Frowen <jamesfrowendev@gmail.com>
2021-12-18 00:47:43 +0000

    adding public extension methods to bypass internal

---
+++ Mirage/Updater.cs
@@ -0,0 +1,14 @@
+namespace Mirage
+{
+    /// <summary>
+    /// Static methods to bypass internal
+    /// <para>
+    /// this class only exists so that we can have the samme NetworkServer class in main repo
+    /// </para>
+    /// </summary>
+    public static class Updater
+    {
+        public static void Update(this NetworkServer server) => server.Update();
+        public static void Update(this NetworkClient client) => client.Update();
+    }
+}
