f9748fd65108b4cb4d0d1fd9852a4b704b66c4f7
James Frowen <jamesfrowendev@gmail.com>
2023-02-13 19:17:36 +0000

    fix: making constructor protected so base classes can be defined without compile error

--- UnityEngine/ScriptableObject.cs
+++ UnityEngine/ScriptableObject.cs
@@ -4,7 +4,7 @@ namespace UnityEngine
 {
     public class ScriptableObject : Object
     {
-        private ScriptableObject() => throw new NotSupportedException();
+        protected ScriptableObject() => throw new NotSupportedException();
 
         public static object CreateInstance<T>() => throw new NotSupportedException();
     }
