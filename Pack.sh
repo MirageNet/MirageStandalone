abbc5e1d26f9684d5cd70abfaaae48ecd0e17419
James Frowen <jamesfrowendev@gmail.com>
2023-10-11 11:29:15 +0100

    nuget publish

--- Pack.sh
+++ Pack.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+# Extract version from AssemblyInfo.cs
+version=$(grep -oP '\[assembly: AssemblyVersion\("\K(.*)(?="\)\])' Mirage/Runtime/AssemblyInfo.cs)
+
+# Update AssemblyVersion and Version in .csproj file
+sed -i "s/<AssemblyVersion>.*<\/AssemblyVersion>/<AssemblyVersion>$version<\/AssemblyVersion>/g" Mirage/Mirage.csproj
+sed -i "s/<Version>.*<\/Version>/<Version>$version<\/Version>/g" Mirage/Mirage.csproj
+
 dotnet publish ./Mirage.CodeGen/Mirage.CodeGen.csproj --configuration Release
-dotnet pack ./Mirage/Mirage.csproj --configuration Release 
+dotnet pack ./Mirage/Mirage.csproj --configuration Release --output out
