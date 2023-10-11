46c323c5f88ce7c40d55399ba7c9fc9971086620
James Frowen <jamesfrowendev@gmail.com>
2023-10-11 23:16:45 +0100

    fix: fixing code gen not including exe

--- Pack.sh
+++ Pack.sh
@@ -6,5 +6,6 @@ version=$(grep -oP '\[assembly: AssemblyVersion\("\K(.*)(?="\)\])' Mirage/Runtim
 sed -i "s/<AssemblyVersion>.*<\/AssemblyVersion>/<AssemblyVersion>$version<\/AssemblyVersion>/g" Mirage/Mirage.csproj
 sed -i "s/<Version>.*<\/Version>/<Version>$version<\/Version>/g" Mirage/Mirage.csproj
 
-dotnet publish ./Mirage.CodeGen/Mirage.CodeGen.csproj --configuration Release
+dotnet publish ./Mirage.CodeGen/Mirage.CodeGen.csproj --configuration Release --runtime win-x64 --self-contained false
+# dotnet publish ./Mirage.CodeGen/Mirage.CodeGen.csproj --configuration Release --runtime linux-x64 --self-contained false
 dotnet pack ./Mirage/Mirage.csproj --configuration Release --output out
